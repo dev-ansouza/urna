5dec828c3af5f1a7e9e51eb8f96c7a2b78e65654
dev_ans <developer.ansouza@gmail.com>
2022-07-23 21:04:46 -0300

    Create js config and initializing window

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,27 @@
 const sound = new Audio();
 
 //Play the sound after touching the key
-function sound_play(number, confirm) {
-    if(number === null || number === ""){
+function sound_play(res) {
+    if(res != 1){
         sound.src = "assets/sounds/confirm.mp3"; 
         sound.play();
     } else {
         sound.src = "assets/sounds/digit.mp3"; 
         sound.play();
     }
+}
+
+function insert(res){
+    const number = document.getElementById('number');
+
+    if(number.textContent.length < 2 ){
+        number.textContent = number.textContent + res;
+        number.style["border"] = "1px solid"
+    }
+}
+
+function fixContent() {
+    const number = document.getElementById('number');
+    number.textContent = "";
+    number.style["border"] = "0px"
 }
